70bc93c02db013091dfda3efa02e787f94e0896f
kko7 <j4kub.kko7@gmail.com>
2020-07-17 23:25:18 +0200

    removed unused components

--- src/components/Buttons/index.js
+++
@@ -1,4 +0,0 @@
-import MenuButton from "./MenuButton";
-import MenuIconButton from "./MenuIconButton";
-
-export { MenuButton, MenuIconButton };
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 import Layout from "./Layout";
 import NavBar from "./NavBar";
 import PluginsList from "./PluginsList";
-import { MenuButton, MenuIconButton } from "./Buttons";
 import Service from "./Service"
 
-export { Layout, NavBar, PluginsList, MenuButton, MenuIconButton, Service };
+export { Layout, NavBar, PluginsList, Service };
