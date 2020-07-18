4b111e01fd481ed3d47507e00e7f550da47d9b71
kko7 <j4kub.kko7@gmail.com>
2020-07-18 17:13:29 +0200

    footer

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
+import {Typography} from '@rmwc/typography'
 
 class Home extends Component {
   render() {
-    return <div>HOME</div>;
+    const { app } = window.require('electron').remote
+    return (
+      <>
+      <Typography tag='h1' style={{ textAlign: "center", marginTop: '10%' }}>League Client Enhancer</Typography>
+
+      <footer style={{ bottom: 0, width: '100%', left: 0, position: 'fixed'}}>
+        <Typography tag='h5' style={{float: "left", paddingLeft: '3%' }}>READY...</Typography> {/* Show real status: connected etc. */}
+        <Typography tag='h5' style={{float: "right", paddingRight: '3%' }}>{`V${app.getVersion() + (!app.isPackaged ? '-BUILD' : '')}`}</Typography>
+      </footer>
+      </>
+    )
   }
 }
 
