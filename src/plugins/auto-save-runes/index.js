2d3bfb12f8ae543b2b94e05fd97b6cac8d9e034d
kko7 <j4kub.kko7@gmail.com>
2020-07-22 12:25:07 +0200

    removed auto-save-runes

--- src/plugins/auto-save-runes/index.js
+++
@@ -1,7 +0,0 @@
-import pluginInfo from "./header"
-import AutoSaveRunesSettings from "./settings"
-import AutoSaveRunesWindow from "./window"
-
-export default pluginInfo
-
-export { AutoSaveRunesSettings, AutoSaveRunesWindow }
--- src/plugins/index.js
+++ src/plugins/index.js
@@ -3,17 +3,10 @@ import DodgeQueueInfo, {
   EasyQueueDodgeSettings,
 } from "./easy-queue-dodge"
 
-import AutoSaveRunesInfo, {
-  AutoSaveRunesSettings,
-  AutoSaveRunesWindow
-} from "./auto-save-runes"
-
 export { 
   EasyQueueDodgeSettings,
-  AutoSaveRunesSettings,
-  AutoSaveRunesWindow
 }
 
 export default function pluginsInfo() {
-  return [DodgeQueueInfo(), AutoSaveRunesInfo()]
+  return [DodgeQueueInfo()]
 }
